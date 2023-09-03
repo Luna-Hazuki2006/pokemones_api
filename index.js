5ff9324c7e701468af6855a7d1ab2b7052b5d7a1
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-09-03 14:50:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/'
+const objetos = []
+const objetosCompletos = []
+
+async function obtenerTodos() {
+    if (localStorage.getItem('pokemones')) {
+        let revisar = JSON.parse(localStorage.getItem('pokemones'))
+        if (revisar != []) {
+
+            return
+        }
+    }    
+    objetos.length = 0
+    objetosCompletos.length = 0
+    const respuesta = await fetch(url)
+    if (!respuesta.ok) {
+        throw new Error(respuesta.statusText)
+    }
+    const data = await respuesta.json()
+    const muchos = [...data['results']]
+    console.log(muchos);
+    for (const dato of muchos) {
+        await obtenerDetalles(dato)
+    }
+    console.log(objetos);
+    almacenar()
+}
+
+async function obtenerDetalles(dato) {
+    const respuesta = await fetch(dato['url'])
+    if (!respuesta.ok) {
+        throw new Error(respuesta.statusText)
+    }
+    const data = await respuesta.json()
+    const habilidades = [...data['abilities']]
+    const items = [...data['held_items']]
+    const movimientos = [...data['moves']]
+    const estadisticas = [...data['stats']]
+    const tipos = [...data['types']]
+    for (let i = 0; i < movimientos.length; i++) {
+        movimientos[i] = movimientos[i]['move']
+    }
+    const pokemon = {
+        nombre: data['name'], 
+        altura: data['height'], 
+        peso: data['weight'], 
+        items: items, 
+        movimientos: movimientos, 
+        habilidades: habilidades, 
+        estadisticas: estadisticas, 
+        tipos: tipos
+    }
+    objetosCompletos.push(data)
+    objetos.push(pokemon)
+}
+
+function almacenar() {
+    const pokemones = JSON.stringify(objetosCompletos)
+    localStorage.setItem('pokemones', pokemones)
+}
+
+obtenerTodos()
